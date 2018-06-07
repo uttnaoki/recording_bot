07c2511d381da8de1e82708c4776c17aa3a6b3c5
Naoki Kinoshita <uttnaoki@gmail.com>
2018-06-07 10:45:53 +0900

    mod my_mention.py: delete unused comment

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -4,24 +4,6 @@ from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
 
-# @respond_to('string')     bot宛のメッセージ
-#                           stringは正規表現が可能 「r'string'」
-# @listen_to('string')      チャンネル内のbot宛以外の投稿
-#                           @botname: では反応しないことに注意
-#                           他の人へのメンションでは反応する
-#                           正規表現可能
-# @default_reply()          DEFAULT_REPLY と同じ働き
-#                           正規表現を指定すると、他のデコーダにヒットせず、
-#                           正規表現にマッチするときに反応
-#                           ・・・なのだが、正規表現を指定するとエラーになる？
-
-# message.reply('string')   @発言者名: string でメッセージを送信
-# message.send('string')    string を送信
-# message.react('icon_emoji')  発言者のメッセージにリアクション(スタンプ)する
-#                               文字列中に':'はいらない
-
-# @xxx() の後は空行がなければどんな関数名でも実行してくれる．
-
 @respond_to('hello')
 def mention_func(message):
     message.reply('hello') # メンション
